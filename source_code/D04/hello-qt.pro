188aac17d8a34adbda0d6eed4acecb4e48e1734a
Matt Chang <chchwy@gmail.com>
2023-09-20 12:05:31 +0800

    Add D04 source code

---
+++ source_code/D04/hello-qt.pro
@@ -0,0 +1,5 @@
+# 這是 hello-qt.pro 的內容
+QT += core gui widgets  # 引入 Qt 模組
+TEMPLATE = app          # 這是一支應用程式 (app)
+TARGET = HelloQt        # 執行檔名稱
+SOURCES = hello_qt.cpp  # 這支專案有一個 CPP
---
+++ source_code/D04/hello_qt.cpp
@@ -0,0 +1,11 @@
+// 這是 hello_qt.cpp 
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QWidget w;
+    w.show();
+    return a.exec();
+}
