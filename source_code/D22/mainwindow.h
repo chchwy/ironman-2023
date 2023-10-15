910fb5a66c1a6a5df27d88f6ba5a93b0518b4dad
Matt Chang <chchwy@gmail.com>
2023-10-16 00:40:16 +0800

    D22

---
+++ source_code/D22/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QDir>
+#include <QScrollArea>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+    void dragEnterEvent(QDragEnterEvent* event) override;
+    void dropEvent(QDropEvent* event) override;
+    void wheelEvent(QWheelEvent* event) override;
+
+    void loadImage(const QString& filepath);
+
+    void openFile();
+    void exitApp();
+    void about();
+
+    void prevImage();
+    void nextImage();
+
+private:
+    QLabel* m_imageLabel = nullptr;
+    QScrollArea* m_scrollArea = nullptr;
+
+    QString m_currentImageName;
+    QDir m_currentDir;
+};
+#endif // MAINWINDOW_H
